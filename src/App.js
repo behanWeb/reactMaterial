a2975df59a54bf5367e0aba4eea6e4074959c081
Amir <eslami.amir76@gmail.com>
2016-10-04 19:16:44 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <Button rmClass="btn" />
+    );
+  }
+}
+
+export default App;
