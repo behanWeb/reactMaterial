fc585b5eb85fdce2e54ea93e30a365ad2e21a141
Amir <eslami.amir76@gmail.com>
2016-10-04 20:52:07 +0330

    add constants.js

---
+++ src/components/constants.js
@@ -0,0 +1,26 @@
+export default {
+  Styles: [
+    'flat',
+    'floating',
+    'large'
+  ],
+  Waves: [
+    'light',
+    'red',
+    'yellow',
+    'orange',
+    'purple',
+    'green',
+    'teal'
+  ],
+  Sizes: [
+    's',
+    'm',
+    'l'
+  ],
+  Aligns: [
+    'left',
+    'right',
+    'center'
+  ]
+}
